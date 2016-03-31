6dfc1a1eba9a505868f695369b9f5dac288a4240
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2016-03-31 23:06:10 +0200

    Add IDialog based bot

--- App.fsx
+++ App.fsx
@@ -1,5 +1,6 @@
 #r "packages/Suave/lib/net40/Suave.dll"
 #r "packages/Microsoft.Bot.Connector/lib/net45/Microsoft.Bot.Connector.dll"
+#r "packages/Microsoft.Bot.Builder/lib/net45/Microsoft.Bot.Builder.dll"
 #r "packages/Microsoft.Rest.ClientRuntime/lib/net45/Microsoft.Rest.ClientRuntime.dll"
 #r "packages/Microsoft.WindowsAzure.ConfigurationManager/lib/net40/Microsoft.WindowsAzure.Configuration.dll"
 #r "packages/Newtonsoft.Json/lib/net45/Newtonsoft.Json.dll"
@@ -13,6 +14,7 @@ open Newtonsoft.Json
 open Newtonsoft.Json.Serialization
 
 open Microsoft.Bot.Connector
+open Microsoft.Bot.Builder.Dialogs
 
 open System
 open System.Threading.Tasks
@@ -36,10 +38,33 @@ module Helpers =
         
     let inline startAsPlainTask (work : Async<unit>) = Task.Factory.StartNew(fun () -> work |> Async.RunSynchronously)
 
-let botHandler (msg : Message) = 
-    msg.CreateReplyMessage(sprintf "You said: %s" msg.Text)
+[<Serializable>]
+type MyBot () =
+   let t = 1
+
+   member this.messageRecived (ctx : IDialogContext) (a : IAwaitable<Message>) = 
+       Task.Factory.StartNew(fun () ->
+           let msg = a.GetAwaiter().GetResult()
+           let t = (sprintf "You said: %s" msg.Text) |> ctx.PostAsync 
+           t.Start ()
+               
+           ctx.Wait <| ResumeAfter(this.messageRecived)
+       )
+
+   interface IDialog with
+       member this.StartAsync ctx = 
+           Task.Factory.StartNew(fun () ->
+               ctx.Wait <| ResumeAfter(this.messageRecived)
+           )
+
+let botHandler (msg : Message) =
+    async {
+        let! m = Conversation.SendAsync(msg, (fun _ -> MyBot () :> IDialog), Threading.CancellationToken()) |> Async.AwaitTask
+        return m
+    } |> Async.RunSynchronously 
 
 let app = 
     path "/api/messages" >=> request (getResourceFromReq >> botHandler >> toJson )
+
 
 startWebServer defaultConfig app
